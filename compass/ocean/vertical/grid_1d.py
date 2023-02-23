5329e1c7588bd4f4e00116795d4d72e8d09d9e29
Alice Barthel <abarthel@lanl.gov>
2023-02-23 11:27:27 -0600

    added print for check on min, max thickness

--- compass/ocean/vertical/grid_1d.py
+++ compass/ocean/vertical/grid_1d.py
@@ -166,9 +166,11 @@ def _create_linear_dz_grid(num_vert_levels, bottom_depth,
     """
 
     nz = num_vert_levels
-
     layerThickness = [(bottom_depth / nz) - (np.floor(nz/2) - k) * linear_dz_rate for k in np.arange(0, nz)]
-
+    min_layer_thickness  = layerThickness[0]
+    max_layer_thickness = layerThickness[-1]
+    print('Linear dz vertical grid')
+    print(f'min layer thickness: {min_layer_thickness}; max layer thickness {max_layer_thickness} in m;')
     interfaces = - np.append([0], np.cumsum(layerThickness))
 
     return interfaces
